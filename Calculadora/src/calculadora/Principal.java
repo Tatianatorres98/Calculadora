adde9dec3f8737ad8bf9009558a03441bc658ad0
Estudiantes <Estudiantes@Labing50420.udistrital.edu.co>
2018-02-09 11:45:58 -0500

    Objeto Calculadora

--- Calculadora/src/calculadora/Principal.java
+++ Calculadora/src/calculadora/Principal.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calculadora;
 
-/**
- *
- * @author Estudiantes
- */
+
 public class Principal {
+
     
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Crear un objeto de la clase Calculadora
+        Calculadora micalculadora; //Definición
+        micalculadora=new Calculadora(); //Construir el objeto
     }
-    
-    
 }
