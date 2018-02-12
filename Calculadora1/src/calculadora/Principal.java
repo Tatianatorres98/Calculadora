46ac87df56bdf7ced9b5c7097d1f3e15e01d3bfd
Estudiantes <Estudiantes@Labing70211>
2018-02-12 16:47:46 -0500

    commit

---
+++ Calculadora1/src/calculadora/Principal.java
@@ -0,0 +1,13 @@
+
+package calculadora;
+
+
+public class Principal {
+
+    
+    public static void main(String[] args) {
+        //Crear un objeto de la clase Calculadora
+        Calculadora micalculadora; //Definición
+        micalculadora=new Calculadora(); //Construir el objeto
+    }
+}
