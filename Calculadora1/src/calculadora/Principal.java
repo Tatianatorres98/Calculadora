b50bae3f1d917d421d3bf0972dc2520474bccd7e
Estudiantes <Estudiantes@Labing70211>
2018-02-12 17:57:42 -0500

    commit

--- Calculadora1/src/calculadora/Principal.java
+++ Calculadora1/src/calculadora/Principal.java
@@ -1,13 +1,37 @@
-
 package calculadora;
 
+import java.util.*;
 
 public class Principal {
 
-    
     public static void main(String[] args) {
-        //Crear un objeto de la clase Calculadora
-        Calculadora micalculadora; //Definición
-        micalculadora=new Calculadora(); //Construir el objeto
+
+        Calculadora micalculadora;
+        micalculadora = new Calculadora();
+        Scanner miescaner = new Scanner(System.in);
+        int opcion;
+        System.out.println("Ingrese opcion");
+        opcion = miescaner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                micalculadora.setX(miescaner.nextDouble());
+                micalculadora.setY(miescaner.nextDouble());
+                micalculadora.sumar();
+                System.out.println("Elresultato es:");
+                System.out.println(micalculadora.getR());
+                
+                break;
+            case 2:
+                
+                micalculadora.setX(miescaner.nextDouble());
+                micalculadora.setY(miescaner.nextDouble());
+                micalculadora.restar();
+                System.out.println("Elresultato es:");
+                System.out.println(micalculadora.getR());
+                break;
+            
+        }
+
     }
 }
